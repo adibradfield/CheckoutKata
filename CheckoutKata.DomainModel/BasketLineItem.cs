fb95a7702396688d77b01af0074c1c6baa4f49f1
Adrian Bradfield <adrian.bradfield@outlook.com>
2020-11-29 22:09:29 +0000

    Added implementation for getting the line price with a promotion

--- CheckoutKata.DomainModel/BasketLineItem.cs
+++ CheckoutKata.DomainModel/BasketLineItem.cs
@@ -6,7 +6,17 @@ namespace CheckoutKata.DomainModel
 
         public int Quantity { get; set; } = 1;
 
-        public decimal LinePrice => StockItem.UnitPrice * Quantity;
+        public decimal LinePrice
+        {
+            get
+            {
+                var basePrice = StockItem.UnitPrice * Quantity;
+                if(StockItem.Promotion != null){
+                    return basePrice - StockItem.Promotion.CalculateDiscountToApply(Quantity);
+                }
+                return basePrice;
+            }
+        }
 
         public BasketLineItem(IStockItem item){
             StockItem = item;
